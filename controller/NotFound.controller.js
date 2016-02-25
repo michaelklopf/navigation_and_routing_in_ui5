6c79ab8cc1dee6d5b81c4ec584fce67efc5c35bd
Michael Klopf <m.klopf@fis-gmbh.de>
2016-02-25 11:54:16 +0000

    Display a target without changing the hash

--- controller/NotFound.controller.js
+++ controller/NotFound.controller.js
@@ -4,6 +4,22 @@ sap.ui.define([
    "use strict";
    return BaseController.extend("sap.ui.demo.nav.controller.NotFound", {
       onInit: function () {
+      	var router, target;
+      	router = this.getRouter();
+      	target = router.getTarget("notFound");
+      	target.attachDisplay(function(evt) {
+      		this._oData = evt.getParameter("data"); // store data for next view?
+      	}, this);
+      },
+      // overwrite parent method
+      onNavBack: function() {
+      	//var history, previousHash, router;
+      	if (this._oData && this._oData.fromTarget) {
+      		this.getRouter().getTargets().display(this._oData.fromTarget);
+      		delete this._oData.fromTarget;
+      		return;
+      	}
+      	BaseController.prototype.onNavBack.apply(this, arguments);
       }
    });
 });
