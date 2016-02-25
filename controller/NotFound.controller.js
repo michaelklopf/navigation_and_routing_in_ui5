df0a3aba6f2b250d28eec219002fbbeaf8fb0250
Michael Klopf <m.klopf@fis-gmbh.de>
2016-02-25 10:08:30 +0000

    Catch invalid hashes with route not found page.

--- controller/NotFound.controller.js
+++ controller/NotFound.controller.js
@@ -2,9 +2,8 @@ sap.ui.define([
    "sap/ui/core/mvc/Controller"
 ], function (Controller) {
    "use strict";
-
-   return Controller.extend("sap.ui.demo.nav.controller.Home", {
-
+   return Controller.extend("sap.ui.demo.nav.controller.NotFound", {
+      onInit: function () {
+      }
    });
-
 });
