f0306a05faad7fc09f4f40232d66047258bad9db
Michael Klopf <m.klopf@fis-gmbh.de>
2016-02-26 11:44:57 +0000

    Making tabs routable with query parameter

--- controller/employees/Resume.controller.js
+++ controller/employees/Resume.controller.js
@@ -1,14 +1,18 @@
 sap.ui.define([
-	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+	"sap/ui/demo/nav/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
 	"use strict";
+	var _validTabKeys = ["Info", "Projects", "Hobbies", "Notes"];
+	
 	return BaseController.extend("sap.ui.demo.nav.controller.employees.Resume", {
 		onInit: function () {
 			var router = this.getRouter();
+			this.getView().setModel(new JSONModel(), "view");
 			router.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
 		},
 		_onRouteMatched : function (oEvent) {
-			var args, view;
+			var args, view, query;
 			args = oEvent.getParameter("arguments");
 			view = this.getView();
 			view.bindElement({
@@ -23,12 +27,32 @@ sap.ui.define([
 					}
 				}
 			});
+			query = args["?query"];
+			if (query && _validTabKeys.indexOf(query.tab) > -1) {
+				view.getModel("view").setProperty("/selectedTabKey", query.tab);
+			} else {
+				this.getRouter().navTo("employeeResume", {
+					employeeId: args.employeeId,
+					query: {
+						tab: _validTabKeys[0]
+					}
+				}, true); // ho history entry
+			}
 		},
 		_onBindingChange : function () {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
 			}
+		},
+		onTabSelect: function(evt) {
+			var ctx = this.getView().getBindingContext();
+			this.getRouter().navTo("employeeResume", {
+				employeeId: ctx.getProperty("EmployeeID"),
+				query: {
+					tab: evt.getParameter("selectedKey")
+				}
+			}, true);
 		}
 	});
 });
